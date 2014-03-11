fb8a0685549cc06e1154d8d3bbb3c84ddea63872
Paul Meagher <meagher43@gmail.com>
2014-03-11 17:55:03 +0000

    All Pauls fine work

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,48 +1,54 @@
 class RacesController < ApplicationController
-  	
-  	def index
+	
+	def index
 
-  		if params.keys.include? "limit"
-  			limit = params[:limit].to_i
-  		else
-  			limit = 10
-  		end
+		if params.keys.include? "limit"
+			limit = params[:limit].to_i
+		else
+			limit = 10
+		end
 
-  		if params.keys.include? "starting_after"
-  			starting_after = DateTime.iso8601(params[:starting_after])
-  		else
-  			starting_after = Time.now
-  		end
-  		
-  		@races = Race.where("start_time > ?",starting_after).limit(limit)
+		if params.keys.include? "starting_after"
+			starting_after = DateTime.iso8601(params[:starting_after])
+		else
+			starting_after = Time.now
+		end
 
-		respond_to do |format|
-			format.json { render json: @races.as_json(include: { user: { only: [ :name , :uuid ] } }, 
-													   except: [ :user_id, :updated_at, :created_at ] ) }
-		end  			
+		@races = Race.where("start_time > ?",starting_after).limit(limit)
 
-	end
+    respond_to do |format|
+      format.json { render json: @races.as_json(include: { user: { only: [ :name , :uuid ] } }, 
+      except: [ :user_id, :updated_at, :created_at ] ) }
+    end  			
+  end
 
-	def show
-	  	@race = Race.find(params[:id])
-  		respond_to do |format|
-  			format.json { render json: @race.as_json(include: { user: { only: [ :name , :uuid ] } }, 
-													  except: [ :user_id, :updated_at, :created_at ] ) }
-  		end
-  	end
-
-  	def create
-
-  		@race = @current_user.races.create( params.slice(:start_time, :distance) )
-     
-      if @race.invalid?
-        render json: @race.errors.messages, status: :bad_request
-        return
+  def show
+    @race = Race.find(params[:id])
+    if params.keys.include? "leaderboard" and ["1","true"].include? params[:leaderboard]
+      respond_to do |format|
+        format.json { render json: @race.as_json(include: { user: { only: [ :name , :uuid ] } }, 
+          except: [ :user_id, :updated_at, :created_at ] ) }
+      end   
+    else
+      respond_to do |format|
+        format.json { render json: @race.as_json(include: { user: { only: [ :name , :uuid ] } }, 
+        except: [ :user_id, :updated_at, :created_at ] ) }
       end
+    end
+  end
 
-  		respond_to do |format|
-  			format.json { render json: @race.as_json(include: { user: { only: [ :name , :uuid ] } }, 
-													  except: [ :user_id, :updated_at, :created_at ] ) }
-  		end
-  	end
+  def create
+
+    @race = @current_user.races.create( params.slice(:start_time, :distance) )
+
+    if @race.invalid?
+      render json: @race.errors.messages, status: :bad_request
+      return
+    end
+
+    respond_to do |format|
+      format.json { render json: @race.as_json(include: { user: { only: [ :name , :uuid ] }, leaderboard:{}}, 
+        except: [ :user_id, :updated_at, :created_at ] ) }
+    end
+  end
 end
