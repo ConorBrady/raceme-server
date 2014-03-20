743987ed502e2a9625959a153a6b3bf637e0186c
Conor Brady <conorjbrady1@gmail.com>
2014-03-20 11:25:50 +0000

    Cleaned up database migrations

--- db/migrate/20140311183328_adding_leader_board.rb
+++
@@ -1,9 +0,0 @@
-class AddingLeaderBoard < ActiveRecord::Migration
-  	def change
-  		create_table "leaderboards", :id => false, :force => true do |t|
-  			t.string "user_race_event_id"
-  			t.decimal "distance_run"
-  			t.timestamps
-  		end
-  	end
-end
